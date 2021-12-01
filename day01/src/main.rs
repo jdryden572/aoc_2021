5d87fa0eacc48d966fb5022e931a428c947e77af
James Dryden <jdryden572@gmail.com>
2021-12-01 16:56:53 -0500

    Use slice::windows() to rework solution. Be more panicky because why not?

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,63 +1,38 @@
 use std::io::{BufRead, BufReader};
-use std::{error::Error, fs::File};
+use std::fs::File;
 
-type EasyResult<T> = Result<T, Box<dyn Error>>;
-
-fn main() -> EasyResult<()> {
-    let answer = part1("input.txt")?;
+fn main() {
+    let answer = part1("input.txt");
     println!("{}", answer);
 
-    let answer_two = part2("input.txt")?;
+    let answer_two = part2("input.txt");
     println!("{}", answer_two);
-
-    Ok(())
 }
 
-fn part1(file_name: &str) -> EasyResult<i32> {
-    let readings = parse_ints_from_file(file_name)?;
-    let mut last_reading = None;
-    let mut increased_reading_count = 0;
-    for reading in readings {
-        if let Some(last) = last_reading {
-            if reading > last {
-                increased_reading_count += 1;
-            }
-        }
-        last_reading = Some(reading);
-    }
-
-    Ok(increased_reading_count)
+fn part1(file_name: &str) -> usize {
+    parse_ints_from_file(file_name)
+        .windows(2)
+        .filter(|pair| pair[0] < pair[1])
+        .count()
 }
 
-fn part2(file_name: &str) -> EasyResult<i32> {
-    let readings = parse_ints_from_file(file_name)?;
-
-    let mut last_reading = None;
-    let mut increased_reading_count = 0;
-    for i in 0..readings.len() - 2 {
-        let slice = &readings[i..i + 3];
-        let reading: i32 = slice.iter().sum();
-        if let Some(last) = last_reading {
-            if reading > last {
-                increased_reading_count += 1;
-            }
-        }
-        last_reading = Some(reading);
-    }
-
-    Ok(increased_reading_count)
+fn part2(file_name: &str) -> usize {
+    parse_ints_from_file(file_name)
+        .windows(3)
+        .map(|w| w.iter().sum())
+        .collect::<Vec<i32>>()
+        .windows(2)
+        .filter(|pair| pair[0] < pair[1])
+        .count()
 }
 
-fn parse_ints_from_file(file_name: &str) -> EasyResult<Vec<i32>> {
-    let file = File::open(file_name)?;
-    let reader = BufReader::new(file).lines();
-    let lines: Result<Vec<_>, _> = reader.collect();
-    let integers: Vec<i32> = lines?
-        .into_iter()
-        .map(|l| l.parse())
-        .collect::<Result<Vec<_>, _>>()?;
-
-    Ok(integers)
+fn parse_ints_from_file(file_name: &str) -> Vec<i32> {
+    let file = File::open(file_name).unwrap();
+    BufReader::new(file)
+        .lines()
+        .map(Result::unwrap)
+        .map(|l| l.parse().unwrap())
+        .collect()
 }
 
 #[cfg(test)]
@@ -66,25 +41,25 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let answer = part1("test_input_1.txt").unwrap();
+        let answer = part1("test_input_1.txt");
         assert_eq!(3, answer);
     }
 
     #[test]
     fn part1_final() {
-        let answer = part1("input.txt").unwrap();
+        let answer = part1("input.txt");
         assert_eq!(1521, answer);
     }
 
     #[test]
     fn test_part2() {
-        let answer = part2("test_input_2.txt").unwrap();
+        let answer = part2("test_input_2.txt");
         assert_eq!(5, answer);
     }
 
     #[test]
     fn part2_final() {
-        let answer = part2("input.txt").unwrap();
+        let answer = part2("input.txt");
         assert_eq!(1543, answer);
     }
 }
