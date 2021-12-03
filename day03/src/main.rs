3869480668cb3b1107810d17f53c87b04946e979
James Dryden <jdryden572@gmail.com>
2021-12-03 12:38:20 -0500

    Rewrite from scratch. Still used bit ops.

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,109 +1,92 @@
 fn main() {
-    let answer_one = part1("input.txt", 12);
+    let answer_one = part1("input.txt");
     println!("Answer one: {}", answer_one);
 
-    let answer_two = part2("input.txt", 12);
+    let answer_two = part2("input.txt");
     println!("Answer two: {}", answer_two);
 }
 
-fn part1(file_name: &str, num_bits: usize) -> usize {
-    let readings = parse_input(file_name).collect();
-    let (sums, count) = get_bit_counts(&readings, num_bits);
+fn part1(file_name: &str) -> u32 {
+    let lines = get_lines(file_name);
+    let num_bits = lines[0].len();
+    let readings = get_readings(lines);
 
-    let mut gamma: usize = 0;
-    let mut epsilon: usize = 0;
+    let mut gamma = 0;
+    let mut epsilon = 0;
+
     for i in 0..num_bits {
-        let bit_set = sums[i] > count / 2;
-        if bit_set {
+        let sum_at_digit = readings
+            .iter()
+            .map(|r| get_nth_digit(*r, i))
+            .sum::<u32>() as f64;
+        
+        let half_count = readings.len() as f64 / 2f64;
+        if sum_at_digit > half_count {
+            // 1 is most common digit
             gamma |= 1 << i;
-        } else {
+        } else if sum_at_digit < half_count {
+            // 0 is most common digit
             epsilon |= 1 << i;
+        } else {
+            panic!("Same number of 0s as 1s, the prompt didn't cover this, I'm fucking panicking");
         }
     }
-
+  
     gamma * epsilon
 }
 
-fn part2(file_name: &str, num_bits: usize) -> usize {
-    let all_readings: Vec<usize> = parse_input(file_name).collect();
+fn part2(file_name: &str) -> u32 {
+    let lines = get_lines(file_name);
+    let num_bits = lines[0].len();
+    let readings = get_readings(lines);
 
-    let oxygen = find_oxygen_reading(all_readings.clone(), num_bits);
-    let co2 = find_co2_reading(all_readings, num_bits);
+    let oxygen = get_rating(readings.clone(), num_bits, true);
+    let co2 = get_rating(readings, num_bits, false);
 
     oxygen * co2
 }
 
-fn find_oxygen_reading(mut readings: Vec<usize>, num_bits: usize) -> usize {
+fn get_rating(mut readings: Vec<u32>, num_bits: usize, oxygen: bool) -> u32 {
     for i in (0..num_bits).rev() {
-        readings = filter_oxygen_by_bit(readings, i);
+        readings = filter_readings(&readings, i, oxygen);
         if readings.len() == 1 {
             break;
         }
-    }   
+    }
 
     readings[0]
 }
 
-fn filter_oxygen_by_bit(readings: Vec<usize>, bit: usize) -> Vec<usize> {
-    let (sums, count) = get_bit_counts(&readings, bit + 1);
+fn filter_readings(readings: &[u32], bit: usize, oxygen: bool) -> Vec<u32> {
+    let sum = readings.iter().map(|r| get_nth_digit(*r, bit)).sum::<u32>() as f64;
+    let half_count = readings.len() as f64 / 2f64;
+    let more_ones = sum >= half_count;
 
-    let iter = readings.into_iter();
+    readings
+        .iter()
+        // this may be the most evil line I've ever written
+        .filter(|&r| ((r & 1 << bit) > 0) ^ oxygen ^ more_ones)
+        .cloned()
+        .collect()
+}
 
-    if sums[bit] as f64 >= (count as f64) / 2f64 {
-        // most common bit is 1 or tie
-        iter.filter(|r| (r & 1 << bit) > 0).collect()
+fn get_lines(file_name: &str) -> Vec<String> {
+    helpers::read_lines_panicky(file_name).collect()
+}
+
+fn get_readings(lines: Vec<String>) -> Vec<u32> {
+    lines
+        .iter()
+        .map(|l| u32::from_str_radix(&l, 2).unwrap())
+        .collect()
+}
+
+fn get_nth_digit(reading: u32, i: usize) -> u32 {
+    if (reading & 1 << i) > 0 {
+        1
     } else {
-        // most common bit is 0
-        iter.filter(|r| (r & 1 << bit) == 0).collect()
+        0
     }
-}
-
-fn find_co2_reading(mut readings: Vec<usize>, num_bits: usize) -> usize {
-    for i in (0..num_bits).rev() {
-        readings = filter_co2_by_bit(readings, i);
-        if readings.len() == 1 {
-            break;
-        }
-    }   
-
-    readings[0]
-}
-
-fn filter_co2_by_bit(readings: Vec<usize>, bit: usize) -> Vec<usize> {
-    let (sums, count) = get_bit_counts(&readings, bit + 1);
-
-    let iter = readings.into_iter();
-
-    if (sums[bit] as f64) < (count as f64) / 2f64 {
-        // least common bit is 1
-        iter.filter(|r| (r & 1 << bit) > 0).collect()
-    } else {
-        // least common bit is 0 or tie
-        iter.filter(|r| (r & 1 << bit) == 0).collect()
-    }
-}
-
-fn parse_input(file_name: &str) -> impl Iterator<Item = usize> + '_ {
-    helpers::read_lines_panicky(file_name)
-        .map(|l| usize::from_str_radix(&l, 2))
-        .map(Result::unwrap)
-}
-
-fn get_bit_counts(readings: &Vec<usize>, num_bits: usize) -> ([usize; 12], usize) {
-    let mut sums: [usize; 12] = [0; 12];
-    let mut count: usize = 0;
-
-    for reading in readings.into_iter() {
-        count += 1;
-        for i in 0..num_bits {
-            let bit_set = (reading & 1 << i) > 0;
-            if bit_set {
-                sums[i] += 1;
-            }
-        }
-    }
-
-    (sums, count)
 }
 
 #[cfg(test)]
@@ -112,21 +95,21 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(198, part1("test_input.txt", 5));
+        assert_eq!(198, part1("test_input.txt"));
     }
 
     #[test]
     fn final_part1() {
-        assert_eq!(4006064, part1("input.txt", 12));
+        assert_eq!(4006064, part1("input.txt"));
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(230, part2("test_input.txt", 5));
+        assert_eq!(230, part2("test_input.txt"));
     }
 
     #[test]
     fn final_part2() {
-        assert_eq!(5941884, part2("input.txt", 12));
+        assert_eq!(5941884, part2("input.txt"));
     }
 }
