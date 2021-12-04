0fd9ec4d657cd7a0455844db5c4e79d91c78ee74
James Dryden <jdryden572@gmail.com>
2021-12-04 10:15:37 -0500

    Solve day 4 part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,9 +1,11 @@
+use std::collections::VecDeque;
+
 fn main() {
     println!("Answer one: {}", part1("input.txt"));
+    println!("Answer two: {}", part2("input.txt"));
 }
 
 fn part1(file_name: &str) -> i32 {
-
     let mut boards = parse_boards(file_name);
     let numbers = parse_called_numbers(file_name);
 
@@ -17,6 +19,31 @@ fn part1(file_name: &str) -> i32 {
     }
 
     panic!("Didn't find a winner!")
+}
+
+fn part2(file_name: &str) -> i32 {
+    let mut boards: VecDeque<Board> = parse_boards(file_name).into();
+    let numbers = parse_called_numbers(file_name);
+
+    for number in numbers {
+        if boards.len() > 1 {
+            for _ in 0..boards.len() {
+                let mut board = boards.pop_front().unwrap();
+                board.mark(number);
+                if !board.bingo() {
+                    boards.push_back(board);
+                }
+            }
+        } else {
+            let board = boards.get_mut(0).unwrap();
+            board.mark(number);
+            if board.bingo() {
+                return board.unmarked_total() * number;
+            }
+        }
+    }
+
+    panic!("Didn't find the last board to win!")
 }
 
 #[derive(Debug)]
@@ -137,5 +164,20 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(4512, part1("test_input.txt"));
+    }
+
+    #[test]
+    fn final_part1() {
+        assert_eq!(69579, part1("input.txt"));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(1924, part2("test_input.txt"));
+    }
+
+    #[test]
+    fn final_part2() {
+        assert_eq!(14877, part2("input.txt"));
     }
 }
