dab4513eacaa4a64ac74dd659bee29f1b8231205
James Dryden <jdryden572@gmail.com>
2021-12-07 11:23:03 -0500

    Clean up last night's mess

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,60 +1,64 @@
-use std::collections::HashMap;
-
 fn main() {
     println!("Answer one: {}", part1("input.txt"));
-    println!("Answer one: {}", part2("input.txt"));
+    println!("Answer two: {}", part2("input.txt"));
 }
 
 fn part1(file_name: &str) -> i32 {
     let line = helpers::read_lines_panicky(file_name).next().unwrap();
-    let positions: Vec<i32> = dbg!(line.split(",").map(|p| p.parse().unwrap()).collect::<Vec<_>>());
+    let crabs: Vec<i32> = line.split(",").map(|p| p.parse().unwrap()).collect::<Vec<_>>();
 
-    let min = *(positions.iter().min().unwrap());
-    let max  = *(positions.iter().max().unwrap());
+    let &min = crabs.iter().min().unwrap();
+    let &max  = crabs.iter().max().unwrap();
 
-    let mut costs = HashMap::new();
-    for i in min..max + 1 {
-        //println!("{}", i);
-        let mut cost = 0;
-        for pos in &positions {
-            cost += (pos - i).abs();
-            //println!("--> {}", cost);
-        }
-        costs.insert(i, cost);
+    let mut min_cost = i32::MAX;
+    for i in min..=max {
+        min_cost = get_new_min(&crabs, i, min_cost);
     }
 
-    let min = costs.iter().min_by(|x, y| x.1.cmp(y.1)).unwrap();
-    *dbg!(min).1
+    min_cost
 }
 
-fn part2(file_name: &str) -> i64 {
+fn get_new_min(crabs: &[i32], position: i32, prev_min: i32) -> i32 {
+    let mut cost = 0;
+    for crab in crabs {
+        cost += (crab - position).abs();
+        if cost > prev_min {
+            return prev_min;
+        }
+    }
+
+    cost
+}
+
+fn part2(file_name: &str) -> i32 {
     let line = helpers::read_lines_panicky(file_name).next().unwrap();
-    let positions: Vec<i64> = dbg!(line.split(",").map(|p| p.parse().unwrap()).collect::<Vec<_>>());
+    let crabs: Vec<i32> = line.split(",").map(|p| p.parse().unwrap()).collect::<Vec<_>>();
 
-    let min = *(positions.iter().min().unwrap());
-    let max  = *(positions.iter().max().unwrap());
+    let &min = crabs.iter().min().unwrap();
+    let &max  = crabs.iter().max().unwrap();
 
-    let mut costs = HashMap::new();
-    for i in min..max + 1 {
-        //println!("{}", i);
-        let mut cost = 0;
-        for pos in &positions {
-            cost += calc((pos - i).abs());
-            //println!("--> {}", cost);
-        }
-        costs.insert(i, cost);
+    let mut min_cost = i32::MAX;
+    for i in min..=max {
+        min_cost = get_new_min_gauss(&crabs, i, min_cost);
     }
 
-    let min = costs.iter().min_by(|x, y| x.1.cmp(y.1)).unwrap();
-    *dbg!(min).1
+    min_cost
 }
 
-fn calc(num: i64) -> i64 {
-    match num {
-        0  => 1,
-        1.. => (1..num+1).sum(),
-        _ => panic!(),
+fn get_new_min_gauss(crabs: &[i32], position: i32, prev_min: i32) -> i32 {
+    let mut cost = 0;
+    for crab in crabs {
+        cost += gauss_sum((crab - position).abs());
+        if cost > prev_min {
+            return prev_min;
+        }
     }
+
+    cost
+}
+
+fn gauss_sum(n: i32) -> i32 {
+    n * (n + 1) / 2
 }
 
 #[cfg(test)]
@@ -67,7 +71,17 @@ mod tests {
     }
 
     #[test]
+    fn final_part1() {
+        assert_eq!(347449, part1("input.txt"));
+    }
+
+    #[test]
     fn test_part2() {
         assert_eq!(168, part2("test_input.txt"));
+    }
+
+    #[test]
+    fn final_part2() {
+        assert_eq!(98039527, part2("input.txt"));
     }
 }
