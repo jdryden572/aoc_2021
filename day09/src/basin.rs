8d8e60cb41ab54609a3f605b29180622e33ef806
James Dryden <jdryden572@gmail.com>
2021-12-11 08:48:06 -0500

    Render gif of basins being discovered

--- day09/src/basin.rs
+++ day09/src/basin.rs
@@ -1,9 +1,9 @@
-use std::collections::HashSet;
+use std::{collections::{HashSet, VecDeque}, iter::FromIterator, hash::Hash};
 
 pub fn part1(file_name: &str) -> u16 {
     let values = parse_values(file_name);
 
-    let matrix = Matrix::new(values);
+    let matrix = Matrix::new_with_low_points(values);
     matrix
         .low_points()
         .into_iter()
@@ -21,20 +21,27 @@ fn fill_basin_recursive(matrix: &Matrix, pos: &Position, basin: &mut HashSet<Pos
 
 pub fn part2(file_name: &str) -> usize {
     let values = parse_values(file_name);
-    let matrix = Matrix::new(values);
+    let mut matrix = Matrix::new_with_low_points(values);
 
-    let mut basins: Vec<HashSet<Position>> = Vec::new();
-    for low_point in matrix.low_points() {
-        let mut basin = HashSet::new();
-        fill_basin_recursive(&matrix, &low_point, &mut basin);
-        basins.push(basin);
+    let mut searchers = matrix.low_points().into_iter().map(BasinSearcher::new).collect::<VecDeque<_>>();
+    let mut finished = Vec::new();
+    while !searchers.is_empty() {
+        for _ in 0..searchers.len() {
+            let mut searcher = searchers.pop_front().unwrap();
+            searcher.step(&mut matrix);
+            if !searcher.is_done() {
+                searchers.push_back(searcher);
+            } else {
+                finished.push(searcher);
+            }
+        }
     }
 
-    basins.sort_by(|x, y| y.len().cmp(&x.len())); // reverse
-    basins.into_iter().take(3).map(|b| b.len()).product()
+    finished.sort_by(|x, y| y.visited.len().cmp(&x.visited.len())); // reverse
+    finished.into_iter().take(3).map(|b| b.visited.len()).product()
 }
 
-fn parse_values(file_name: &str) -> Vec<Vec<u8>> {
+pub fn parse_values(file_name: &str) -> Vec<Vec<u8>> {
     helpers::read_lines_panicky(file_name)
         .map(|l| {
             l.chars()
@@ -44,49 +51,107 @@ fn parse_values(file_name: &str) -> Vec<Vec<u8>> {
         .collect::<Vec<_>>()
 }
 
-struct Matrix {
+pub struct BasinSearcher {
+    low_point: Position,
+    visited: HashSet<Position>,
+    frontier: Vec<Position>,
+}
+
+impl BasinSearcher {
+    pub fn new(low_point: Position) -> Self {
+        Self {
+            low_point,
+            visited: HashSet::new(),
+            frontier: Vec::from_iter([low_point]),
+        }
+    }
+
+    pub fn step(&mut self, matrix: &mut Matrix) {
+        if let Some(mut pos) = self.frontier.pop() {
+            if pos.kind != PositionType::LowPoint {
+                pos.kind = PositionType::InBasin;
+            }
+            if self.visited.insert(pos) {
+                if pos.kind != PositionType::LowPoint {
+                    matrix.position_mut(pos.x, pos.y).kind = PositionType::InBasin;
+                }
+                let neighbors_in_basin = matrix.neighbors(&pos).into_iter().filter(|p| p.kind != PositionType::HighPoint);
+                self.frontier.extend(neighbors_in_basin);
+            }
+        }
+    }
+
+    pub fn is_done(&self) -> bool {
+        self.frontier.is_empty()
+    }
+}
+
+pub struct Matrix {
     max_x: usize,
     max_y: usize,
     positions: Vec<Vec<Position>>,
 }
 
 impl Matrix {
-    fn new(values: Vec<Vec<u8>>) -> Self {
+    pub fn new_with_low_points(values: Vec<Vec<u8>>) -> Self {
         let max_x = values[0].len() - 1;
         let max_y = values.len() - 1;
         let mut positions = vec![Vec::new(); max_y + 1];
         for y in 0..=max_y {
             for x in 0..=max_x {
+                let val = values[y][x];
                 positions[y].push(Position {
                     x,
                     y,
-                    val: values[y][x],
+                    val,
+                    kind: PositionType::from(val)
                 });
             }
         }
-        Self {
+        let mut matrix = Self {
             max_x,
             max_y,
             positions,
-        }
+        };
+        matrix.mark_low_points();
+        matrix
     }
 
-    fn position(&self, x: usize, y: usize) -> Position {
+    pub fn iter(&self) -> impl Iterator<Item = &Position> {
+        self.positions.iter().flat_map(|row| row.iter())
+    }
+
+    fn position_copy(&self, x: usize, y: usize) -> Position {
         self.positions[y][x]
     }
 
-    fn low_points(&self) -> Vec<Position> {
-        let mut low_points = Vec::new();
+    fn position_mut(&mut self, x: usize, y: usize) -> &mut Position {
+        self.positions.get_mut(y).unwrap()
+            .get_mut(x).unwrap()
+    }
+
+    fn update_position(&mut self, pos: Position) {
+        self.positions[pos.y][pos.x] = pos;
+    }
+
+    fn mark_low_points(&mut self) {
         for x in 0..=self.max_x {
             for y in 0..=self.max_y {
-                let pos = self.position(x, y);
+                let mut pos = self.position_copy(x, y);
                 let neighbors = self.neighbors(&pos);
                 if pos.val < neighbors.iter().map(|p| p.val).min().unwrap() {
-                    low_points.push(pos);
+                    pos.kind = PositionType::LowPoint;
+                    self.update_position(pos);
                 }
             }
         }
-        low_points
+    }
+
+    pub fn low_points(&self) -> Vec<Position> {
+        self.iter()
+            .filter(|&p| p.kind == PositionType::LowPoint)
+            .copied()
+            .collect()
     }
 
     fn neighbors(&self, pos: &Position) -> Vec<Position> {
@@ -107,11 +172,36 @@ impl Matrix {
     }
 }
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+    pub val: u8,
+    pub kind: PositionType,
+}
+
+impl Hash for Position {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+    }
+}
+
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
-struct Position {
-    x: usize,
-    y: usize,
-    val: u8,
+pub enum PositionType {
+    Unknown,
+    LowPoint,
+    InBasin,
+    HighPoint,
+}
+
+impl From<u8> for PositionType {
+    fn from(val: u8) -> Self {
+        match val {
+            9 => Self::HighPoint,
+            _ => Self::Unknown,
+        }
+    }
 }
 
 #[cfg(test)]
--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,6 +1,70 @@
+use std::{collections::VecDeque, time::Instant};
+
+use plotters::{prelude::*, coord::Shift};
+
 mod basin;
+use basin::{Matrix, parse_values, BasinSearcher, Position, PositionType};
 
 fn main() {
-    println!("Answer one: {}", basin::part1("input.txt"));
-    println!("Answer two: {}", basin::part2("input.txt"));
+    let drawing_area = BitMapBackend::gif(
+        "images/animated.gif", 
+        (1000, 1000), 
+        1_00  /* Each frame show .1s */
+    ).unwrap().into_drawing_area();
+
+    let values = parse_values("input.txt");
+    let mut matrix = Matrix::new_with_low_points(values);
+
+    let mut step = 0;
+    render(&drawing_area, &mut matrix, step);
+
+    let mut searchers = matrix.low_points().into_iter().map(BasinSearcher::new).collect::<VecDeque<_>>();
+    let mut finished = Vec::new();
+    while !searchers.is_empty() {
+        let start = Instant::now();
+        step += 1;
+        print!("Step {}, {} basins remaining. ", step, searchers.len());
+        for _ in 0..searchers.len() {
+            let mut searcher = searchers.pop_front().unwrap();
+            searcher.step(&mut matrix);
+            if !searcher.is_done() {
+                searchers.push_back(searcher);
+            } else {
+                finished.push(searcher);
+            }
+        }
+        let calc_done = Instant::now();
+        print!("Calc[{:?}] ", calc_done - start);
+        render(&drawing_area, &mut &matrix, step);
+        println!("Render[{:?}]", Instant::now() - calc_done);
+    }
+}
+
+fn render(drawing_area: &DrawingArea<BitMapBackend, Shift>, matrix: &Matrix, step: i32) {
+    drawing_area.fill(&WHITE).unwrap();
+
+    let mut ctx = ChartBuilder::on(&drawing_area)
+        .build_cartesian_2d(0.0..100.0, 0.0..100.0)
+        .unwrap();
+
+    ctx.draw_series(matrix.iter().map(rect_for)).unwrap();
+
+    ctx.configure_mesh().draw().unwrap();
+
+    let text = format!("{}", step);
+    let style = TextStyle::from(("sans-serif", 20).into_font()).color(&RED);
+    drawing_area.draw_text(&text, &style, (0, 0)).unwrap();
+    drawing_area.present().unwrap();
+}
+
+fn rect_for(pos: &Position) -> Rectangle<(f64, f64)> {
+    //println!("{},{}", pos.x, pos.y);
+    let (x, y) = (pos.x as f64, pos.y as f64);
+    let color = match pos.kind {
+        PositionType::LowPoint => RED.mix(0.5).filled(),
+        PositionType::InBasin => YELLOW.mix(0.5).filled(),
+        PositionType::HighPoint => GREEN.mix(0.5).filled(),
+        PositionType::Unknown => WHITE.mix(1.0).filled(),
+    };
+    Rectangle::new([(x, y), (x + 1.0, y + 1.0)], color)
 }
