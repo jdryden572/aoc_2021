0c66d84c56bfc0e70cafc32cb05aa6d2a5100e36
James Dryden <jdryden572@gmail.com>
2021-12-13 09:00:27 -0500

    Solve day 13 part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -4,11 +4,11 @@ type Coord = (u32, u32);
 
 fn main() {
     println!("Answer one: {}", part1("input.txt"));
+    println!("Answer two: \r\n{}", part2("input.txt"));
 }
 
 fn part1(file_name: &str) -> usize {
-    let mut coords: HashSet<Coord> = parse_coordinates(file_name)
-        .collect();
+    let mut coords: HashSet<Coord> = parse_coordinates(file_name).collect();
 
     let fold = parse_folds(file_name).next().unwrap();
 
@@ -20,8 +20,46 @@ fn part1(file_name: &str) -> usize {
     coords.len()
 }
 
+fn part2(file_name: &str) -> String {
+    let mut coords: HashSet<Coord> = parse_coordinates(file_name).collect();
+
+    for fold in parse_folds(file_name) {
+        match fold {
+            Fold::X(x) => fold_x(x, &mut coords),
+            Fold::Y(y) => fold_y(y, &mut coords),
+        }
+    }
+
+    let x_max = coords.iter().map(|&(x, _)| x).max().unwrap() as usize;
+    let y_max = coords.iter().map(|&(_, y)| y).max().unwrap();
+
+    let empty = " ";
+    let filled = "#";
+    let mut grid = Vec::new();
+    for _ in 0..=y_max {
+        grid.push(vec![empty; x_max + 1]);
+    }
+
+    for (x, y) in coords {
+        let (x, y) = (x as usize, y as usize);
+        grid[y][x] = filled;
+    }
+
+    let mut code = String::from("\n");
+    for row in grid {
+        code.push_str(&row.join(""));
+        code.push_str("\n");
+    }
+
+    code
+}
+
 fn fold_x(fold_x: u32, coords: &mut HashSet<Coord>) {
-    let temp: Vec<Coord> = coords.iter().filter(|&&(x, _)| x > fold_x).cloned().collect();
+    let temp: Vec<Coord> = coords
+        .iter()
+        .filter(|&&(x, _)| x > fold_x)
+        .cloned()
+        .collect();
     for coord in temp {
         let (mut x, y) = coords.take(&coord).unwrap();
         x = fold_x - (x - fold_x);
@@ -30,7 +68,11 @@ fn fold_x(fold_x: u32, coords: &mut HashSet<Coord>) {
 }
 
 fn fold_y(fold_y: u32, coords: &mut HashSet<Coord>) {
-    let temp: Vec<Coord> = coords.iter().filter(|&&(_, y)| y > fold_y).cloned().collect();
+    let temp: Vec<Coord> = coords
+        .iter()
+        .filter(|&&(_, y)| y > fold_y)
+        .cloned()
+        .collect();
     for coord in temp {
         let (x, mut y) = coords.take(&coord).unwrap();
         y = fold_y - (y - fold_y);
@@ -80,5 +122,30 @@ mod tests {
     #[test]
     fn final_part1() {
         assert_eq!(751, part1("input.txt"));
+    }
+
+    #[test]
+    fn test_part2() {
+        let expected = "
+#####
+#   #
+#   #
+#   #
+#####
+";
+        assert_eq!(expected, part2("test_input.txt"));
+    }
+
+    #[test]
+    fn final_part2() {
+        let expected = "
+###   ##  #  # ###  #  # #    #  # #   
+#  # #  # #  # #  # # #  #    # #  #   
+#  # #    #### #  # ##   #    ##   #   
+###  # ## #  # ###  # #  #    # #  #   
+#    #  # #  # # #  # #  #    # #  #   
+#     ### #  # #  # #  # #### #  # ####
+";
+        assert_eq!(expected, part2("input.txt"));
     }
 }
