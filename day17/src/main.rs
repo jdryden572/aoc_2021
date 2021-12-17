fc2fabe983e4f37789a11ddf550cddaa126a30ac
James Dryden <jdryden572@gmail.com>
2021-12-17 11:04:49 -0500

    Solve day 17 part 2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,27 +1,51 @@
-use std::cmp::max;
+use std::{cmp::max, time::Instant};
 
 // target area: x=25..67, y=-260..-200
-const TARGET: &TargetArea = &TargetArea { 
+const TARGET: &TargetArea = &TargetArea {
     min_x: 25,
     max_x: 67,
     min_y: -260,
-    max_y: -200
+    max_y: -200,
 };
 
 fn main() {
-    println!("Answer one: {}", part1(TARGET));
+    let start = Instant::now();
+    println!(
+        "Answer one: {} ({:?})",
+        part1(TARGET),
+        Instant::now() - start
+    );
+
+    let start = Instant::now();
+    println!(
+        "Answer two: {} ({:?})",
+        part2(TARGET),
+        Instant::now() - start
+    );
 }
 
 fn part1(target: &TargetArea) -> i32 {
     let mut max_y = i32::MIN;
-    for x_vel in 1..10000 {
-        for y_vel in 1..10000 {
+    for x_vel in 1..68 {
+        for y_vel in 1..1000 {
             if let Some(height) = get_height_if_hit(x_vel, y_vel, target) {
                 max_y = max(max_y, height);
             }
         }
     }
     max_y
+}
+
+fn part2(target: &TargetArea) -> usize {
+    let mut hits = 0;
+    for x_vel in 1..68 {
+        for y_vel in -261..1000 {
+            if let Some(_) = get_height_if_hit(x_vel, y_vel, target) {
+                hits += 1;
+            }
+        }
+    }
+    hits
 }
 
 fn get_height_if_hit(x_vel: i32, y_vel: i32, target: &TargetArea) -> Option<i32> {
@@ -49,9 +73,9 @@ struct Trajectory {
 impl Trajectory {
     fn new(x_vel: i32, y_vel: i32) -> Self {
         Self {
-            x_vel, 
-            y_vel, 
-            position: (0, 0)
+            x_vel,
+            y_vel,
+            position: (0, 0),
         }
     }
 }
@@ -70,7 +94,6 @@ impl Iterator for Trajectory {
     }
 }
 
-
 struct TargetArea {
     min_x: i32,
     max_x: i32,
@@ -80,8 +103,7 @@ struct TargetArea {
 
 impl TargetArea {
     fn contains(&self, x: i32, y: i32) -> bool {
-        self.min_x <= x && x <= self.max_x &&
-            self.min_y <= y && y <= self.max_y
+        self.min_x <= x && x <= self.max_x && self.min_y <= y && y <= self.max_y
     }
 
     fn is_past(&self, x: i32, y: i32) -> bool {
@@ -93,11 +115,11 @@ impl TargetArea {
 mod tests {
     use super::*;
 
-    const TEST_TARGET: &TargetArea = &TargetArea { 
+    const TEST_TARGET: &TargetArea = &TargetArea {
         min_x: 20,
         max_x: 30,
         min_y: -10,
-        max_y: -5
+        max_y: -5,
     };
 
     #[test]
@@ -108,5 +130,15 @@ mod tests {
     #[test]
     fn final_part1() {
         assert_eq!(33670, part1(TARGET));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(112, part2(TEST_TARGET));
+    }
+
+    #[test]
+    fn final_part2() {
+        assert_eq!(4903, part2(TARGET));
     }
 }
