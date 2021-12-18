697963e5b4895382e0c3befbe3d17e88a457aba8
James Dryden <jdryden572@gmail.com>
2021-12-18 12:28:35 -0500

    Finish exploded logic

--- day18/src/explode.rs
+++ day18/src/explode.rs
@@ -1,46 +1,49 @@
 use crate::pair::*;
+use Status::*;
 
 pub fn explode(pair: &mut Pair) -> bool {
-    explode_pair_recurs(pair, 0);
+    let status = explode_pair_recurs(pair, 0);
 
-    true
+    matches!(status, Exploded(..))
 }
 
-fn explode_pair_recurs(pair: &mut Pair, depth: usize) -> (Option<usize>, Option<usize>) {
-    let (left_0, left_1) = explode_element_recurs(&mut pair.0, depth);
-    let mut escape = false;
-    if let Some(add) = left_1 {
-        escape = true;
-        add_number_to_left(&mut pair.1, add);
-    }
-
-    if left_0.is_some() {
-        return (left_0, None);
-    }
-
-    if escape {
-        return (None, None);
-    }
-
-    let (right_0, right_1) = explode_element_recurs(&mut pair.1, depth);
-    if let Some(add) = right_0 {
-        add_number_to_right(&mut pair.0, add);
-    }
-
-    if right_1.is_some() {
-        return (None, right_1);
-    }
-
-    (None, None)
+enum Status {
+    Exploded(Option<usize>, Option<usize>),
+    NotExploded,
 }
 
-fn explode_element_recurs(element: &mut Element, depth: usize) -> (Option<usize>, Option<usize>) {
+
+fn explode_pair_recurs(pair: &mut Pair, depth: usize) -> Status {
+    match explode_element_recurs(&mut pair.0, depth) {
+        Exploded(left_0, left_1) => {
+            if let Some(add) = left_1 {
+                add_number_to_left(&mut pair.1, add);
+            }
+        
+            return Exploded(left_0, None)
+        },
+        NotExploded => {},
+    }
+
+    match explode_element_recurs(&mut pair.1, depth) {
+        Exploded(right_0, right_1) => {
+            if let Some(add) = right_0 {
+                add_number_to_right(&mut pair.0, add);
+            }
+
+            Exploded(None, right_1)
+        },
+        NotExploded => NotExploded,
+    }
+}
+
+fn explode_element_recurs(element: &mut Element, depth: usize) -> Status {
     match element {
-        Element::Number(_) => (None, None),
+        Element::Number(_) => NotExploded,
         Element::Pair(pair) => if depth == 3 {
-            let result = (pair.0.number(), pair.1.number());
+            let (left, right) = (pair.0.number(), pair.1.number());
             *element = Element::Number(0);
-            result
+            Exploded(left, right)
         } else {
             explode_pair_recurs(pair, depth + 1)
         },
@@ -99,6 +102,21 @@ mod tests {
             let mut pair = parse_pair(input);
             assert!(explode(&mut pair));
             assert_eq!(expected, &format!("{}", pair));
+        }
+    }
+
+    #[test]
+    fn test_not_exploded() {
+        let cases = vec![
+            "[1,2]",
+            "[[1,2],3]",
+            "[9,[8,7]]",
+            "[[1,9],[8,5]]",
+        ];
+        for input in cases {
+            let mut pair = parse_pair(input);
+            assert!(!explode(&mut pair));
+            assert_eq!(input, &format!("{}", pair));
         }
     }
 }
